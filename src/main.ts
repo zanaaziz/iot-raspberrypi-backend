6fa28578ba9cc56ba9afe7d41097a8e8abe8f92b
Zana Aziz <mail@zanaaziz.com>
2024-12-02 11:54:44 +0000

    enabled swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.useGlobalPipes(new ValidationPipe());
+
+	const config = new DocumentBuilder().setTitle('API Docs').build();
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('docs', app, document);
+
 	await app.listen(process.env.PORT ?? 3000);
 }
 
